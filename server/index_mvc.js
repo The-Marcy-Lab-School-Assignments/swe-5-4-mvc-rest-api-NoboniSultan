f18ab233ccbbe9856071ad6a5fc234c17915518d
NoboniSultan <sultannoboni@gmail.com>
2026-03-23 10:46:53 -0400

    Fixed bugs

--- server/index_mvc.js
+++ server/index_mvc.js
@@ -9,14 +9,14 @@ const logRoutes = (req, res, next) => {
 };
 
 app.use(logRoutes);
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 app.use(express.json());
 
-app.get('./api.todos', todoControllers.listTools);
+app.get('/api.todos', todoControllers.listTodos);
 app.get('/api/todos/:id', todoControllers.findTodo);
-app.post('./api/todo', todoControllers.createTodo);
-app.patch('./api/todos/:id', todoControllers.updateTodo);
-app.delete('./api/todos/:id', todoControllers.deleteTodo);
+app.post('/api/todo', todoControllers.createTodo);
+app.patch('/api/todos/:id', todoControllers.updateTodo);
+app.delete('/api/todos/:id', todoControllers.deleteTodo);
 
 app.use((req, res) => {
   res.status(404).json({ message: `Error: Not found ${req.url}` });
